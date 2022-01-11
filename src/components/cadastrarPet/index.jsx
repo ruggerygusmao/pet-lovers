d54aa9c65e82ccdcc7f1956e18bf6872f1d38b7d
ruggerygusmao <ruggery402@gmail.com>
2022-01-11 00:11:56 -0300

    correcao no cadastrar pet

--- src/components/cadastrarPet/index.jsx
+++ src/components/cadastrarPet/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Buttons } from '../cadastrarUsuario/styles';
 import Nav from '../nav';
 import { Form } from './styles';
@@ -8,13 +8,14 @@ import { axiosApi } from '../../services/axios';
 export default function CadastrarPet() {
     const [nome, setNome] = useState("");
     const [photoUrl, setPhotoUrl] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async ev => {
         ev.preventDefault();
         try {
             const pet = { name: nome, photo_url: photoUrl }
             await axiosApi.post('pets', pet);
-            window.location = "/home";
+            navigate("/home");
         } catch (error) {
             console.log(error)
         }
@@ -25,12 +26,8 @@ export default function CadastrarPet() {
             <Nav />
             <Form onSubmit={handleSubmit}>
                 <div>
-                    <label>Nome</label>
-                    <input type="text" name="nome" id="nome" onChange={(e) => setNome(e.target.value)} required />
-                </div>
-                <div>
                     <label>Raça</label>
-                    <input type="text" name="raca" id="raca"  />
+                    <input type="text" name="nome" id="nome" onChange={(e) => setNome(e.target.value)} required />
                 </div>
                 <div>
                     <label>Idade</label>
@@ -44,12 +41,8 @@ export default function CadastrarPet() {
                     <label>Endereço entrega</label>
                     <input type="text" name="endereco" id="endereco"  />
                 </div>                <div>
-                    <label>URL de foto</label>
+                    <label>URL da foto</label>
                     <input type="text" name="photo_url" id="photo_url" onChange={(e) => setPhotoUrl(e.target.value)} required />
-                </div>
-                <div>
-                    <label>Foto</label>
-                    <input type="file" name="img" id="img" />
                 </div>
                 <div>
                     <label>Descrição</label>
